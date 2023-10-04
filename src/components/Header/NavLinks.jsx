de53403d4e609792879defda23d47b92692da6f0
Rama Astra <ramaastra333@gmail.com>
2023-10-05 03:29:02 +0800

    Removing About navlink in header, set Discover navlink to be active on detail page

--- src/components/Header/NavLinks.jsx
+++ src/components/Header/NavLinks.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import classes from './NavLinks.module.css'
 
 const links = [
   { label: 'Home', href: '/' },
-  { label: 'About', href: '/about' },
   { label: 'Discover', href: '/discover' }
 ]
 
 export default function NavLinks() {
   const location = useLocation()
   const [active, setActive] = useState(location.pathname)
+
+  useEffect(() => {
+    setActive(
+      location.pathname.includes('/discover')
+        ? '/discover'
+        : location.pathname
+    )
+  }, [location.pathname])
 
   return (
     <>
